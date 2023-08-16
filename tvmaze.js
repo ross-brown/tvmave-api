b003a1c39eba522c6887356ee19ea2d089950d69
ross-brown <rbrown_98@yahoo.com>
2023-08-16 14:28:09 -0500

    add todo comments

--- tvmaze.js
+++ tvmaze.js
@@ -4,6 +4,7 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+//TODO: global for tinyurl
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -11,13 +12,12 @@ const $searchForm = $("#searchForm");
  *    Each show object should contain exactly: {id, name, summary, image}
  *    (if no image URL given by API, put in a default image URL)
  */
-
+//TODO: const data = res.json()
+//TODO: get only needed data out
 async function getShowsByTerm(term) {
   const params = new URLSearchParams({ q: term });
 
-  const response = await fetch(
-    `https://api.tvmaze.com/search/shows?${params}`
-  );
+  const response = await fetch(`https://api.tvmaze.com/search/shows?${params}`);
 
   return await response.json();
 }
